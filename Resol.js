97e7a8be8145a067f2bf55747895ee2b00380ee2
theo.rogue <theo.rogue@hotmail.fr>
2020-06-05 01:00:42 +0200

    rajout d'info sur ticket+changement d'état

---
+++ Resol.js
@@ -0,0 +1,13 @@
+function resol() {
+    document.getElementById("encours").style.display = "none";
+    document.getElementById("resol").style.display = "block";
+    document.getElementById("attente").style.display = "none";
+    document.getElementById('id01').style.display='none';
+};
+
+function encours() {
+    document.getElementById("encours").style.display = "block";
+    document.getElementById("resol").style.display = "none";
+    document.getElementById("attente").style.display = "none";
+    document.getElementById('id01').style.display='none';
+};
